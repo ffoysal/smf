74895d4cf1f4fac094cb4f2c42a0fd6a2715c809
Foysal <foysal.iqbal.fb@gmail.com>
2015-09-18 16:04:01 -0400

    added birth_day, birth_month, birth_year auto save

--- admin/controllers/smf.php
+++ admin/controllers/smf.php
@@ -14,4 +14,16 @@ class SmfControllerSmf extends JControllerForm
 {
 	//after save, cancel redirecto to this view
 	protected $view_list="datalist";
+
+ // Call automatically by joomla
+	public function save(){
+		$data  = $this->input->post->get('jform', array(), 'array');		
+		$date = new JDate($data['birth_date']);
+		$data['birth_day'] =$date->day;
+		$data['birth_month'] =$date->month;
+		$data['birth_year'] =$date->year;
+		//set the values again to store into the db
+		$this->input->post->set('jform',$data);
+		parent::save();
+	}	
 }
