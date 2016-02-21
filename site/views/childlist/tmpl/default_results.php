6fb7562d1b1ba0b69d64b5099e8533dbb4a9bd4c
Foysal Iqbal <foysal.iqbal.fb@gmail.com>
2016-02-21 14:36:59 -0500

    add sponsor button

--- site/views/childlist/tmpl/default_results.php
+++ site/views/childlist/tmpl/default_results.php
@@ -37,12 +37,36 @@ defined('_JEXEC') or die;
 	      		</div>
 			    <div class="row">
 			    	<div class="col-md-4 smf-paypal-button-position">
-			          	<div style="width: 171px;height: 80px;" class="s5" data-state="hasContent" id="i478zcpg" data-reactid=".0.$SITE_ROOT.$desktop_siteRoot.$SITE_HEADER.1.1.$i478zcpg">
+			          	<!-- div style="width: 171px;height: 80px;" class="s5" data-state="hasContent" id="i478zcpg" data-reactid=".0.$SITE_ROOT.$desktop_siteRoot.$SITE_HEADER.1.1.$i478zcpg">
 			          		<div id="i478zcpgiFrameHolder" class="s5iFrameHolder" data-reactid=".0.$SITE_ROOT.$desktop_siteRoot.$SITE_HEADER.1.1.$i478zcpg.0">
 			          			<iframe width="100%" style="border:none;" height="93px" src="http://www.sabrinamemorial.org.usrfiles.com/html/4c2bb9_02316a46c772b8c8d485a64b61f62e6e.html" data-reactid=".0.$SITE_ROOT.$desktop_siteRoot.$SITE_HEADER.1.1.$i478zcpg.0.0">
 			          			</iframe>
 			          		</div>
-			          	</div>
+			          	</div -->
+						<?php if ($this->escape($result->sponsored)) { ?>
+								<span class="label label-success already_sponsored">Already Sponsored</span>
+						<?php } else { ?>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_donations">
+						<!-- Replace "business" value with your PayPal Email Address or Account ID -->
+						<input type="hidden" name="business" value="sabrinamemorial@gmail.com">
+						<input type="hidden" name="lc" value="US">
+						<input type="hidden" name="item_name" value="Sabrina Memorial Foundation (<?php echo $this->escape($result->student_id);?>)">
+
+						<input type="hidden" name="no_note" value="0">
+						<input type="hidden" name="cn" value="Any message to Sabrina Memorial Foundation:">
+						<input type="hidden" name="no_shipping" value="2">
+						<input type="hidden" name="currency_code" value="USD">
+						<input type="hidden" name="max_text" value="Yearly 240 and 20 per month">
+
+						<input type="hidden" name="return" value="http://www.smfacademy.co/smf">
+						<input type="hidden" name="cancel_return" value="http://www.smfacademy.co/smf">
+
+						<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
+						<input type="image" src="images/smf/sponsor_now_btn.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+						</form>
+						<?php } ?>						
 			        </div>
 			    </div>
   			</div>
